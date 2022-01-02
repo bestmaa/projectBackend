9ecebee31e522b274e4f0112208c9c6c88b792ed
bestmaa <bestemplin@gmail.com>
2022-01-02 16:16:28 +0530

    fl

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import dotenv from "dotenv";
 import cloudinary from "cloudinary";
 import connectmongodb from "./config/database.js";
-const PRODUCTMOOD="DEV"
+const PRODUCTMOOD="pro"
 process.on("uncaughtException", (err) => {
   console.log(`"Error: ${err.message}`);
   console.log(`Shut down server due to uncaughtExceptioon rejection`);
@@ -12,14 +12,16 @@ if(PRODUCTMOOD=="DEV"){
   dotenv.config({ path: "./config/config.env" });
 
 }
+
 connectmongodb();
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-const server = app.listen(process.env.PORT, () => {
-  console.log("Server Start" + " " + process.env.PORT);
+const ports=process.env.PORT || 4000
+const server = app.listen(ports, () => {
+  console.log("Server Start" + " " + ports);
 });
 //unhandled Promise Rejection
 // process.on("unhandledRejection",err=>{
